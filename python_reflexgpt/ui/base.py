7bef74460272ca372be5aaee7c7c1e633f1bac73
Darren <d1r22fgh98@gmail.com>
2024-08-31 22:11:48 +0800

    udpate ui

--- python_reflexgpt/ui/base.py
+++ python_reflexgpt/ui/base.py
@@ -1,8 +1,14 @@
 import reflex as rx
 from .navbar import base_navbar
+from .footer import base_footer
 
 def base_layout(*args, **kwargs) -> rx.Component:
     return rx.container(
         base_navbar(),
-        *args, **kwargs
+        rx.fragment(
+            id = "my-base-fragment",
+            *args, 
+            **kwargs,),
+        base_footer(),
+        id = "my-base-container"   
     )
