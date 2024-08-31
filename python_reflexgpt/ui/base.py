15d027d6f7cddbf8bbd4b20c328b13ddf7a524c9
Darren <d1r22fgh98@gmail.com>
2024-08-31 15:36:14 +0800

    ui

---
+++ python_reflexgpt/ui/base.py
@@ -0,0 +1,8 @@
+import reflex as rx
+from .navbar import base_navbar
+
+def base_layout(*args, **kwargs) -> rx.Component:
+    return rx.container(
+        base_navbar("Navbar"),
+        *args, **kwargs
+    )
---
+++ python_reflexgpt/ui/navbar.py
@@ -0,0 +1,5 @@
+import reflex as rx
+
+def base_navbar(child, *args, **kwargs) -> rx.Component:
+
+    return rx.heading(child, *args, **kwargs)
