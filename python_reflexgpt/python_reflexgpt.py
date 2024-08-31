d4d85dad7b1a196bf9110f7209c2b1e343173d80
Darren <d1r22fgh98@gmail.com>
2024-08-31 14:46:55 +0800

    reflex init

--- python_reflexgpt/python_reflexgpt.py
+++ python_reflexgpt/python_reflexgpt.py
@@ -11,12 +11,12 @@ class State(rx.State):
     ...
 
 
-def index() -> rx.Component:
-    # Welcome Page (Index)
+def about_us() -> rx.Component:
+    """About Us"""
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading("Welcome to Reflex GPT!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
@@ -34,6 +34,29 @@ def index() -> rx.Component:
         rx.logo(),
     )
 
+def index() -> rx.Component:
+    """Index"""
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex GPT!", size="9"),
+            rx.text(
+                "Get started by editing something alike",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+        rx.logo(),
+    )
 
 app = rx.App()
-app.add_page(index)
+app.add_page(index, route="/")
+app.add_page(about_us, route="/about")
