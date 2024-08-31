345b7e260355f62301efeab4bd836ea8f0ab71ba
Darren <d1r22fgh98@gmail.com>
2024-08-31 15:36:30 +0800

    pages

--- python_reflexgpt/python_reflexgpt.py
+++ python_reflexgpt/python_reflexgpt.py
@@ -1,62 +1,13 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-
-from rxconfig import config
-
+from . import ui, pages
 
 class State(rx.State):
     """The app state."""
 
     ...
 
-
-def about_us() -> rx.Component:
-    """About Us"""
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex GPT!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
-def index() -> rx.Component:
-    """Index"""
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex GPT!", size="9"),
-            rx.text(
-                "Get started by editing something alike",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
 app = rx.App()
-app.add_page(index, route="/")
-app.add_page(about_us, route="/about")
+app.add_page(pages.home_page, route="/")
+app.add_page(pages.about_us_page, route="/about")
